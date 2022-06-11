f1a36d6443a3ee864e8eebe3f22aa93cb5a72dc4
sukanyagovindan <gsukanya@gmail.com>
2022-06-10 22:19:37 -0700

    commit tree e6c1aa494aa3a6ff1adb667ef10695f94bb71be4

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(customerLocation){
+    return Math.abs(customerLocation-42);
+}
+
+function distanceFromHqInFeet(customerLocation){
+    return distanceFromHqInBlocks(customerLocation)*264;
+}
+function distanceTravelledInFeet(start,destination){
+    return Math.abs((destination-start)*264);
+}
+
+
+function calculatesFarePrice(start, destination) {
+    var distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+        return 0;
+    } else if (distance > 400 && distance <= 2000) {
+        return 0.02 * (distance-400);
+    } else if (distance > 2000 && distance <= 2500) {
+        return 25;
+    } else if (distance>2500) {
+        return "cannot travel that far";
+    }
+
+}
